d2088dbbed03453756b69ed8f33639e8951fbf11
Alkida Vaci <alkida.vaci@gmail.com>
2022-12-10 18:06:25 -0500

    GET request for api notes

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-//! GIVEN a note-taking application
-//! WHEN I open the Note Taker
-//! THEN I am presented with a landing page with a link to a notes page
-//! WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+//! WHEN I enter a new note title and the note’s text
+//! THEN a Save icon appears in the navigation at the top of the page
+//! WHEN I click on the Save icon
+//! THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
 // WHEN I click on an existing note in the list in the left-hand column
 // THEN that note appears in the right-hand column
 // WHEN I click on the Write icon in the navigation at the top of the page
@@ -16,6 +11,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
 
 // Initial port
 const PORT = process.env.PORT || 3001;
@@ -35,6 +33,21 @@ app.use(express.static('public'));
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
+// GET request for api notes
+app.get('/api/notes', (req, res) => {
+    // Read data from db.json
+    let db = fs.readFileSync("./db/db.json", "utf8");
+    
+    // Parse data
+    let data = JSON.parse(db);
+    
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+    
+    // Send a message to the client
+    res.status(200).json(data);
+  });
 
 // All routs that don't exist fallback to home page index.html
 app.get('*', (req, res) =>
