0092f233bda5259af0eb810d6db8a7c53999f69b
Alkida Vaci <alkida.vaci@gmail.com>
2022-12-10 16:51:38 -0500

    Html GET request for notes page. Roots that don't exists fallback to home page.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
+//! GIVEN a note-taking application
+//! WHEN I open the Note Taker
+//! THEN I am presented with a landing page with a link to a notes page
+//! WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 // WHEN I enter a new note title and the note’s text
 // THEN a Save icon appears in the navigation at the top of the page
@@ -17,6 +17,10 @@ const express = require('express');
 const path = require('path');
 const fs = require("fs");
 
+// Initial port
+const PORT = process.env.PORT || 3001;
+
+
 // Create instance of express
 const app = express();
 
@@ -26,3 +30,18 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware to serve Public folder to express
 app.use(express.static('public'));
+
+// Html GET request
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+// All routs that don't exist fallback to home page index.html
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Listen for connections
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
