09f29a562f73707ccae1875098433894b8d2fdbb
Alkida Vaci <alkida.vaci@gmail.com>
2022-12-10 22:11:27 -0500

    DELETE request for notes saved on database

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-//! WHEN I enter a new note title and the note’s text
-//! THEN a Save icon appears in the navigation at the top of the page
-//! WHEN I click on the Save icon
-//! THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
 // Packages require
 const express = require('express');
 const path = require('path');
@@ -29,10 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to serve Public folder to express
 app.use(express.static('public'));
 
+
+
 // Html GET request
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
 
 // GET request for api notes
 app.get('/api/notes', (req, res) => {
@@ -49,10 +43,76 @@ app.get('/api/notes', (req, res) => {
     res.status(200).json(data);
   });
 
+
+  // POST request to add a note
+  app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // New note from request body
+    const newNote = req.body;
+  
+    // Add unique id for each note taken
+    newNote.id = uuid();
+      
+      // If all the required properties are present
+    if (newNote) {
+        
+      // Obtain existing notes
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {         
+            console.error(err);
+        } else {
+          // Convert string into JSON object
+          parsedNotes = JSON.parse(data);
+          
+          // Add a new note
+          parsedNotes.push(newNote);
+        
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+           );
+         }
+       });
+          
+       res.status(201).json(newNote);
+     } else {
+       res.status(500).json('Error in posting review');
+     }
+   });
+
+ // DELETE request for notes saved on database
+ app.delete("/api/notes/:id", (req, res) => { 
+
+    // Request id 
+    const deleteNoteId = req.params.id
+    // Read db file and  parse data
+    let dbData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    
+    //Filter array of db data, deleting note with id requested  
+    const dbDataUpdate = dbData.filter( note => note.id !== deleteNoteId )
+    
+    // Write file after deleting the requested note
+    fs.writeFile('./db/db.json', JSON.stringify(dbDataUpdate),
+       (Err) =>
+         Err
+           ? console.error(Err)
+           : console.info('Successfully updated notes!')
+    );
+    res.status(201).json(dbDataUpdate)
+ });
+
+
 // All routs that don't exist fallback to home page index.html
-app.get('*', (req, res) =>
+app.get('*', (req, res) =>{
 res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+});
 
 // Listen for connections
 app.listen(PORT, () =>
